c8b8c0eb770a370b8ba3012ee05c14f3ab4c3595
pawan <pawankumar.s@karmanya.co.in>
2016-04-19 17:56:51 +0530

    replace text

--- submit.php
+++ submit.php
@@ -3,7 +3,7 @@ if(isset($_POST['submit'])){
     $find=$_POST['find'];
     $find=explode(",",$find);
     array_walk($find,'addDelimiter');
-    $replace= explode(",", $_POST["replace"]);
+    $replace= preg_split('/,\s+/', $_POST['replace']);
     $textarea=$_POST['textarea'];
    
         $result= preg_replace($find, $replace, $textarea);
